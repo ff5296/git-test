fad1dd8f9f83c176080bc95980f6ded337eda41f
努力努力再努力 <531136732@qq.com>
2021-09-10 10:24:22 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,9 +1,14 @@
 package com.atguigu.git;
 
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello.git!");
         System.out.println("hello.git!");
         System.out.println("hello.git3!");
+        System.out.println("hello.git4!");
     }
+
+
+
 }
